ef237cbe5af174fb6439e162b0b3829f2f79888a
Irshad Ali <ia46994@gmail.com>
2024-10-24 08:23:45 +0530

    video details page create

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -29,13 +29,13 @@ const VideoCard = ({ video }) => {
                         <span className='text-sm font-bold line-clamp-2'>
                             {video?.title}
                         </span>
-                        <span className='text-[12px] font-semibold mt-2 text-white/[0.7 flex items-center] '>
+                        <span className='text-[12px] lg:text-[10px] xl:text-[12px] font-semibold mt-2 text-white/[0.7] flex items-center '>
                             {video?.author?.title}
                             {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
-                                <BsFillCheckCircleFill className='text-white/[0.5] text-[12px] ml-1 ' />
+                                <BsFillCheckCircleFill className='text-white/[0.5] text-[12px] lg:text-[10px] xl:text-[12px] ml-1 ' />
                             )}
                         </span>
-                        <div className="flex text-[12px] font-semibold text-white/[0.7] truncate overflow-hidden">
+                        <div className="flex text-[12px] lg:text-[10px] font-semibold text-white/[0.7] truncate overflow-hidden">
                             <span>{`${abbreviateNumber(video?.stats?.views, 2)} views`}
                             </span>
                             <span className='flex text-[24px] leading-none font-bold  text-white/[0.7] relative top-[-10px] mx-1'>.
