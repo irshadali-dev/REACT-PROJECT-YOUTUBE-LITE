af9822a9536a06633e4eb26f35bc066d953a7830
Irshad Ali <ia46994@gmail.com>
2024-12-04 15:57:41 +0530

    Api key change

--- src/shared/VideoLength.jsx
+++ src/shared/VideoLength.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 
 const VideoLength = ({ time }) => {
 
-    const VideoLengthInSeconds = moment().startOf("day").seconds(time).format("H:mm:ss")
+    const VideoLengthInSeconds = moment()?.startOf("day")?.seconds(time)?.format("H:mm:ss")
 
     return (
         <div className='absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md'>
